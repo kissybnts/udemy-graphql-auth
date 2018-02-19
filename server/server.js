47b8fd8bd633c54a9342e5f1c932f368ef8fed00
Masataka Kishida <fstt.fwmym.f15@gmail.com>
2018-02-19 22:58:57 +0900

    Lecture 79, MLab setup

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ const schema = require('./schema/schema');
 const app = express();
 
 // Replace with your mongoLab URI
-const MONGO_URI = '';
+const MONGO_URI = 'mongodb://kishida:masataka@ds241658.mlab.com:41658/udemy-graphql-auth';
 
 // Mongoose's built in promise library is deprecated, replace it with ES2015 Promise
 mongoose.Promise = global.Promise;
