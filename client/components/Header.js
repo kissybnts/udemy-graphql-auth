a3d04015a037ed76ede6ebbec51622065cda48e5
Masataka Kishida <fstt.fwmym.f15@gmail.com>
2018-02-22 23:33:04 +0900

    Lecture 91, Authentication State

--- client/components/Header.js
+++ client/components/Header.js
@@ -3,10 +3,29 @@ import { graphql } from 'react-apollo';
 import query from '../queries/CurrentUser';
 
 class Header extends Component {
+  renderButtons() {
+    const { loading, user } = this.props.data;
+
+    if (loading) {
+      return <div/>;
+    }
+
+    if (user) {
+      return <div>Logout</div>;
+    } else {
+      return (
+        <div>You're not signed in.</div>
+      );
+    }
+  }
+
   render() {
-    console.log(this.props);
     return (
-      <div>Header</div>
+      <nav>
+        <div className="nav-wrapper">
+          {this.renderButtons()}
+        </div>
+      </nav>
     );
   }
 }
