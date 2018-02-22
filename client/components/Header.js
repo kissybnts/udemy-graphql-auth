5cbca695dcc076aff5081c857a3e835b9af3926c
Masataka Kishida <fstt.fwmym.f15@gmail.com>
2018-02-22 23:15:47 +0900

    Lecture 89, Figuring Out the Current User

--- client/components/Header.js
+++ client/components/Header.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
+import { graphql } from 'react-apollo';
+import query from '../queries/CurrentUser';
 
 class Header extends Component {
   render() {
+    console.log(this.props);
     return (
       <div>Header</div>
     );
   }
 }
 
-export default Header;
+export default graphql(query)(Header);
