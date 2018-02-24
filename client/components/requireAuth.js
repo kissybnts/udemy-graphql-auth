2c10f6518b3a26433a1bc90e00f3010d3d047fd6
Masataka Kishida <fstt.fwmym.f15@gmail.com>
2018-02-24 11:48:42 +0900

    Lecture 111, Getting Started with RequireAuth

---
+++ client/components/requireAuth.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { graphql } from 'react-apollo';
+import { hashHistory } from 'react-router';
+
+import currentUserQuery from '../queries/CurrentUser';
+
+class RequireAuth extends Component {
+  componentDidMount() {
+    if (!this.props.data.loading && !this.props.data.user) {
+      hashHistory.push('login');
+    }
+  }
+}
+
+export default graphql(currentUserQuery)(RequireAuth);
